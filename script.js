929ed2618a64af8f93173bd19f3c786a10e4a59a
umartahir-99 <tahirumar835@gmail.com>
2024-12-21 20:06:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Write a program that asks the user to input either 1 (turn on) or 0 (turn off).
+
+
+var userInput =prompt("Enter a number:")
+if(userInput == 1){
+   console.log("The light is Turn ON");
+  
+}
+else if(userInput == 0){
+   console.log("The light is Turn Off");
+ 
+}
+else{
+    console.log("Invalid Input")
+ 
+}
+
